a6c0f46e48b5d1e682cbebd5f2e32e8188059c1e
Matteo Meroni <75049430+matteomeroni14@users.noreply.github.com>
2022-02-23 14:00:08 -0800

    Create Youtube_Downloader_To_Mp3.py

---
+++ Youtube_Downloader_To_Mp3.py
@@ -0,0 +1,22 @@
+# Import the module
+from pytube import YouTube
+import os
+
+print("\nYoutube To Mp3 Format\n")
+
+URL = input("Enter Video URL: ")
+yt = YouTube(URL)
+
+try:
+    print("\nDownloading....")
+    video= yt.streams.filter(only_audio = True).first()
+    out_file = video.download()
+    
+    base, ext = os.path.splitext(out_file)
+    new_file = base + ".mp3"
+    os.rename(out_file, new_file)
+    print("\nSuccessfully Downloaded\n")
+    
+except:
+    print("\nSomething Went Wrong! Please Try Again")
+    
